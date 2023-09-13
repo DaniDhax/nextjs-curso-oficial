f788246227024f8aea0a92c025a79d1c83e69458
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-09-13 08:57:49 +0000

    Dynamic Routes. Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
@@ -28,6 +28,7 @@ export default function Post({ postData }) {
             <br />
             {postData.date}
             <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     );
 }
